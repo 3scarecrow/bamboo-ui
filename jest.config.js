a678f82ea62f68019b677fabe6441d25f7e7fc0f
3scarecrow <1648314523@qq.com>
2024-09-28 21:44:25 +0800

    test: 调整测试用例配置

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,14 @@ module.exports = {
   testEnvironment: 'jsdom',
   testURL: 'https://jest.test',
   moduleFileExtensions: ['js', 'ts'],
-  testMatch: ['<rootDir>/packages/**/__test__/**/*.test.{js,ts}'],
+  testMatch: ['<rootDir>/packages/**/__test__/*.test.{js,ts}'],
+  transform: {
+    "^.+\\.js?$": "babel-jest", // Adding this line solved the issue
+    // "^.+\\.ts?$": "ts-jest"
+  },
   collectCoverageFrom: [
-      '<rootDir>/src/**/*.{js,ts}',
-      '!<rootDir>/src/weui-wxss/**',
+      '<rootDir>/packages/**/*.{js,ts}',
+      '!<rootDir>/packages/weui-wxss/**',
       '!**/__test__/**'
   ],
   snapshotSerializers: ['miniprogram-simulate/jest-snapshot-plugin']
